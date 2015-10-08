9dd62b1472bd8596fde0afb6d06c9da6fc2a949c
Rafael Belo <rafaelrpbelo@gmail.com>
2015-10-08 18:04:02 -0300

    RSpec clean tmp folder before and after the tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'fileutils'
+require 'digest'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -5,5 +8,11 @@ RSpec.configure do |config|
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
+  end
+
+  config.around(:example) do |example|
+    FileUtils.rm_r Dir.glob('./tmp/*')
+    example.run
+    FileUtils.rm_r Dir.glob('./tmp/*')
   end
 end
